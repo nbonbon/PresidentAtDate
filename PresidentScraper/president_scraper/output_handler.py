8c5fbd0f61ef9b4bbf8fc0f105c852202da17737
nick bonnet <njbuwm@gmail.com>
2025-02-07 12:57:03 -0500

    Add .csv to president at date data. Rename data.json to presidents.json like others.

--- PresidentScraper/president_scraper/output_handler.py
+++ PresidentScraper/president_scraper/output_handler.py
@@ -13,7 +13,7 @@ class OutputHandler:
             terms_json.append(term.to_dict())
 
         # Specify the file path
-        file_path = 'data\data.json'
+        file_path = 'data\presidents.json'
 
         # Write JSON data to a file
         with open(file_path, 'w') as json_file:
